43c2c041cf34b15436e9587b2eac0ca3cf7da57a
GunjanUpadhyay <gunjanupadhyay9837@gmail.com>
2024-03-25 22:25:41 +0530

    Upadte on 25 march

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,5 +3,6 @@ import {login} from "../controllers/user.js"
 const app=express.Router();
 
 app.get("/login",login);
+//console.log('user side');
 
-export default app;            
+export default app;         
