f1b2c14f3e9982f0075320855ce22ffed05cc6bb
GunjanUpadhyay <gunjanupadhyay9837@gmail.com>
2024-04-05 22:18:11 +0530

    5 April Update

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,31 +1,62 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { addMembers, deleteChat, getChatDetails, getMessages, getMyChats, getMyGroups, leaveGroup, newGroupChat, removeMember, renameGroup, sendAttachments } from "../controllers/chat.js";
+import {
+  addMembers,
+  deleteChat,
+  getChatDetails,
+  getMessages,
+  getMyChats,
+  getMyGroups,
+  leaveGroup,
+  newGroupChat,
+  removeMember,
+  renameGroup,
+  sendAttachments,
+} from "../controllers/chat.js";
 import { attachmentsMulter } from "../middlewares/multer.js";
+import {
+  addMemberValidator,
+  chatIdValidator,
+  newGroupValidator,
+  removeMemberValidator,
+  renameValidator,
+  sendAttachmentsValidator,
+  validateHandler,
+} from "../lib/validators.js";
 
-const app=express.Router();
-
+const app = express.Router();
 
 // Here Logged in user
 
 app.use(isAuthenticated);
 
+app.post("/new", newGroupValidator(), validateHandler, newGroupChat);
+app.get("/my", getMyChats);
+app.get("/my/groups", getMyGroups);
+app.put("/addmembers", addMemberValidator(), validateHandler, addMembers);
+app.put(
+  "/removemember",
+  removeMemberValidator(),
+  validateHandler,
+  removeMember
+);
+app.delete("/leave/:id", chatIdValidator, validateHandler, leaveGroup);
+app.post(
+  "/message",
+  attachmentsMulter,
+  sendAttachmentsValidator(),
+  validateHandler,
+  sendAttachments
+);
 
-app.post("/new",newGroupChat)
-app.get("/my",getMyChats);
-app.get("/my/groups",getMyGroups)
-app.put("/addmembers",addMembers)
-app.put("/removemember",removeMember)
-app.delete("/leave/:id",leaveGroup)
-app.post("/message",attachmentsMulter,sendAttachments)
+app.get("/message/:id", chatIdValidator(), validateHandler, getMessages);
 
+// chats ki details
 
-app.get("/message/:id",getMessages)
+app
+  .route("/:id")
+  .get(chatIdValidator(), validateHandler, getChatDetails)
+  .put(renameValidator(),validateHandler,renameGroup)
+  .delete(chatIdValidator(),validateHandler,deleteChat);
 
-// chats ki details 
-
-app.route("/:id").get(getChatDetails).put(renameGroup).delete(deleteChat);
-
-export default app;         
-                                    
- 
+export default app;
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,13 +2,15 @@ import  express  from "express";
 import {login, getMyProfile,newUser, logout,searchUser} from "../controllers/user.js"
 import {singleAvatar} from "../middlewares/multer.js"
 import { isAuthenticated } from "../middlewares/auth.js";
+import { loginValidator, registerValidator, validateHandler } from "../lib/validators.js";
 
 const app=express.Router();
 
 // here not a login user
 
-app.post("/new",singleAvatar, newUser);
-app.post("/login",login);
+
+app.post("/new",singleAvatar, registerValidator(),validateHandler,newUser);
+app.post("/login",loginValidator(),validateHandler,login);
 
 
 // Here Logged in user
