fcbfbd3ad4b7d794a09a68963ee8575ccce4abf8
GunjanUpadhyay <gunjanupadhyay9837@gmail.com>
2024-04-18 23:55:05 +0530

    18 april

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,26 +1,60 @@
-import  express  from "express";
-import {login, getMyProfile,newUser, logout,searchUser} from "../controllers/user.js"
-import {singleAvatar} from "../middlewares/multer.js"
+import express from "express";
+import {
+  acceptFriendRequest,
+  getMyFriends,
+  getMyNotifications,
+  getMyProfile,
+  login,
+  logout,
+  newUser,
+  searchUser,
+  sendFriendRequest,
+} from "../controllers/user.js";
+import {
+  acceptRequestValidator,
+  loginValidator,
+  registerValidator,
+  sendRequestValidator,
+  validateHandler,
+} from "../lib/validators.js";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { loginValidator, registerValidator, validateHandler } from "../lib/validators.js";
+import { singleAvatar } from "../middlewares/multer.js";
 
-const app=express.Router();
+const app = express.Router();
+// // here not a login user
 
-// here not a login user
+app.post("/new", singleAvatar, registerValidator(), validateHandler, newUser);
+app.post("/login", loginValidator(), validateHandler, login);
 
-
-app.post("/new",singleAvatar, registerValidator(),validateHandler,newUser);
-app.post("/login",loginValidator(),validateHandler,login);
-
-
-// Here Logged in user
+// After here user must be logged in to access the routes
+// // Here Logged in user
 
 app.use(isAuthenticated);
 
 app.get("/me", getMyProfile);
-app.get("/logout", logout);
-app.get("/search",searchUser);
 
-export default app;         
+app.get("/logout", logout);
+
+app.get("/search", searchUser);
+
+app.put(
+  "/sendrequest",
+  sendRequestValidator(),
+  validateHandler,
+  sendFriendRequest
+);
+
+app.put(
+  "/acceptrequest",
+  acceptRequestValidator(),
+  validateHandler,
+  acceptFriendRequest
+);
+
+app.get("/notifications", getMyNotifications);
+
+app.get("/friends", getMyFriends);
+
+export default app;    
                                     
  
