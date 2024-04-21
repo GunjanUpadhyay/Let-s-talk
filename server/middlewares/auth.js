fe552234d427d588d5b265eb732aa1b65669e9e4
GunjanUpadhyay <gunjanupadhyay9837@gmail.com>
2024-04-21 22:52:10 +0530

    21 April

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -33,29 +33,29 @@ const isAuthenticated = TryCatch((req, res, next) => {
 //   next();
 // };
 
-const socketAuthenticator = async (err, socket, next) => {
-  try {
-    if (err) return next(err);
+// const socketAuthenticator = async (err, socket, next) => {
+//   try {
+//     if (err) return next(err);
 
-    const authToken = socket.request.cookies[CHATTU_TOKEN];
+//     const authToken = socket.request.cookies[CHATTU_TOKEN];
 
-    if (!authToken)
-      return next(new ErrorHandler("Please login to access this route", 401));
+//     if (!authToken)
+//       return next(new ErrorHandler("Please login to access this route", 401));
 
-    const decodedData = jwt.verify(authToken, process.env.JWT_SECRET);
+//     const decodedData = jwt.verify(authToken, process.env.JWT_SECRET);
 
-    const user = await User.findById(decodedData._id);
+//     const user = await User.findById(decodedData._id);
 
-    if (!user)
-      return next(new ErrorHandler("Please login to access this route", 401));
+//     if (!user)
+//       return next(new ErrorHandler("Please login to access this route", 401));
 
-    socket.user = user;
+//     socket.user = user;
 
-    return next();
-  } catch (error) {
-    console.log(error);
-    return next(new ErrorHandler("Please login to access this route", 401));
-  }
-};
+//     return next();
+//   } catch (error) {
+//     console.log(error);
+//     return next(new ErrorHandler("Please login to access this route", 401));
+//   }
+// };
 
 export { isAuthenticated, socketAuthenticator };
